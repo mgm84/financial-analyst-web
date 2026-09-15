9124546342acf3aac3fd22330f413f79afb14c43
mgm84 <ferdinand.ls@gmail.com>
2026-09-15 19:30:03 +0200

    Update screen_percentiles_scheduled.py

--- screen_percentiles_scheduled.py
+++ screen_percentiles_scheduled.py
@@ -42,7 +42,7 @@ CARTERA = {
 }
 
 CONVERSION = {
-    "TSM":  {"fx": "USDTWD", "invertir": False, "adr": 1},
+    "TSM":  {"fx": "USDTWD", "invertir": False, "adr": 5},
     "ASML": {"fx": "EURUSD", "invertir": True,  "adr": 1},
 }
 
